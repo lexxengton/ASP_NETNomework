5ed2260b36d2eae1e54fa11e063a6aa136181bd2
Alexey Shotin <lexxengton@inbox.ru>
2020-10-18 12:13:29 +0300

    HomeWork2
    Завершенная вторая домашняя работа

--- NewModels/SecondHomeWorkController.cs
+++ NewModels/SecondHomeWorkController.cs
@@ -12,5 +12,52 @@ namespace WebStore.NewModels
         {
             return View();
         }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+
     }
 }
